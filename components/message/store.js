1dac409a764b48b693e6309b17b86fb9b9d2c533
Juan Pablo Calao <jcalaop@gmail.com>
2020-07-26 08:32:28 -0500

    connect DB and replace read and write functions

--- components/message/store.js
+++ components/message/store.js
@@ -1,11 +1,26 @@
+const db = require('mongoose')
+const Model = require('./model')
+const dotenv = require('dotenv')
+const model = require('./model')
+dotenv.config()
+
+db.Promise = global.Promise
+db.connect(process.env.DB_URI, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true
+})
+console.log('[DB] Conectada con éxito')
+
 const list = []
 
 function addMessage(message) {
-	list.push(message)
+	const myMessage = new Model(message)
+	myMessage.save()
 }
 
-function getMessagesList() {
-	return list
+async function getMessagesList() {
+	const messages = await model.find()
+	return messages
 }
 
 module.exports = {
